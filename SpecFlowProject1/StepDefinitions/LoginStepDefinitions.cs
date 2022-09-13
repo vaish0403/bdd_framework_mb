7beeb8a41042ebfe5652b561a720aac0a0ea136d
Vaishnavi Shahaji Patil <57799996+vaish0403@users.noreply.github.com>
2022-09-13 17:28:35 +0530

    dependency injection

--- SpecFlowProject1/StepDefinitions/LoginStepDefinitions.cs
+++ SpecFlowProject1/StepDefinitions/LoginStepDefinitions.cs
@@ -9,21 +9,22 @@ namespace Specflow_Automation.StepDefinitions
     [Binding]
     public class LoginStepDefinitions
     {
-        
-        [Given(@"I have browser with orangehrm page")]
-        public void GivenIHaveBrowserWithOrangehrmPage()
+
+        [Given(@"I have a browser and orangehrm page")]
+        public void GivenIHaveABrowserAndOrangehrmPage()
         {
             AutomationHooks.driver = new ChromeDriver();
             AutomationHooks.driver.Manage().Window.Maximize();
-            AutomationHooks.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
             AutomationHooks.driver.Url = "https://opensource-demo.orangehrmlive.com/";
-        } 
+
+            AutomationHooks.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
 
         [When(@"I enter username as '([^']*)'")]
         public void WhenIEnterUsernameAs(string username)
         {
             AutomationHooks.driver.FindElement(By.Name("username")).SendKeys(username);
-           // Console.Write(username);
         }
 
         [When(@"I enter password as '([^']*)'")]
@@ -31,22 +32,35 @@ namespace Specflow_Automation.StepDefinitions
         {
             AutomationHooks.driver.FindElement(By.Name("password")).SendKeys(password);
         }
-        [When(@"I click on login")]
+
+        [When(@"I click on Login")]
         public void WhenIClickOnLogin()
         {
-            AutomationHooks.driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+            AutomationHooks.driver.FindElement(By.CssSelector("button[type=submit]")).Click();
         }
 
-        [Then(@"I should be navigate to '([^']*)' dashboard screen")]
-        public void ThenIShouldBeNavigateToDashboardScreen(string expectedValue)
+        [Then(@"I should be successfully logged in")]
+        public void ThenIShouldBeSuccessfullyLoggedIn()
         {
-            Console.WriteLine(expectedValue);
+            Console.WriteLine("Logged in successfully");
         }
+
+        [Then(@"I shoud be navigated to '([^']*)' dashboard screen")]
+        public void ThenIShoudBeNavigatedToDashboardScreen(string pIM)
+        {
+            Console.WriteLine("navigated to dashboard screen");
+        }
+
+        [Then(@"I should not be able to log in")]
+        public void ThenIShouldNotBeAbleToLogIn()
+        {
+            Console.WriteLine("Not able to log in");
+        }
+
         [Then(@"I should get an error message as '([^']*)'")]
-        public void ThenIShouldGetAnErrorMessageAs(string errormessage)
+        public void ThenIShouldGetAnErrorMessageAs(string p0)
         {
-            Console.WriteLine(errormessage);
+            Console.WriteLine("Show an error message");
         }
-
     }
 }
